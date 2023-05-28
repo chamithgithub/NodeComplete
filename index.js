f5c7d75af0bb35e3daf94982854ca886f414ff8e
Cha1552164 <chamithgithub>
2023-05-28 11:27:57 +0530

    new import method to use packege

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const os =require("os");
+// const os =require("os");
+// const path =require('path');
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename =fileURLToPath(import.meta.url);
 
 console.log(os.platform());
 console.log(os.version());
-console.log(__dirname);
+
+// console.log(__dirname);
 console.log(__filename);
+
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
