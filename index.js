18026beed659524798d8aa97d984413d2814b98e
Cha1552164 <chamithgithub>
2023-05-28 11:45:20 +0530

    new import method to use packege update

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 // const path =require('path');
 import os from "os";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";       //es module scop type
 const __filename =fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
 
 console.log(os.platform());
 console.log(os.version());
 
-// console.log(__dirname);
+console.log(__dirname);
 console.log(__filename);
 
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
