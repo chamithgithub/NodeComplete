4b0e65ef893652f843ef9e8ab06043c6c3bbde67
Cha1552164 <dchamith4@gmail.com>
2023-06-05 10:10:48 +0530

    myfunction js add

---
+++ myFunction.js
@@ -0,0 +1,14 @@
+const math = (Number1,Number2,Number3,Number4,)=>{
+    let a=Number1+Number2*2;
+    if(Number3){
+        a+Number3-5
+    }
+    
+    if(Number4){
+        a*=Number4
+
+    }
+    return a
+}
+console.log(math(2,3,5,5));
+export default math;
