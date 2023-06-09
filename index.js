fb3eed273558875e5cfeb3d37ceea7b9d21fce49
Cha1552164 <dchamith4@gmail.com>
2023-06-09 12:52:28 +0530

    update node import

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { dirname, join } from 'path';
-import { readFile } from "fs";
-import {fileURLToPath}from "url";
+import { dirname, join } from 'node:path';
+import { readFile } from "node:fs";
+import {fileURLToPath}from "node:url";
+import { log } from 'node:console';
 
 const __filename =fileURLToPath(import.meta.url);
 const __dirname =dirname(__filename);
 
-readFile(join(__dirname,"read.txt"),/*"utf-8",*/(err,data) =>{
+readFile(join(__dirname,"read.txt"),"utf-8",(err,data) =>{
     if(err){
         console.log(err);
     }
-    console.log(data.toString());
+log(data)
 });
