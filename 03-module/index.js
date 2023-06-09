ea514028be8fe25bd9211ef3db80aa39c1d59de2
Cha1552164 <dchamith4@gmail.com>
2023-06-09 13:23:12 +0530

    update module c

--- 03-module/index.js
+++ 03-module/index.js
@@ -1,18 +1,24 @@
 import { isUtf8 } from "node:buffer";
 import { log } from "node:console";
-import { readFile } from "node:fs";
+import { readFile, writeFile } from "node:fs";
 import { dirname, join } from "node:path";
 import {fileURLToPath} from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname =dirname(__filename);
 
-readFile(join(__dirname,"read.txt"),{
-    encoding:'Utf-8'
-},(err,data)=>{
-    if(!err){
-        log(data);
-    }
+// readFile(join(__dirname,"read.txt"),{
+//     encoding:'Utf-8'
+// },(err,data)=>{
+//     if(!err){
+//         log(data);
+//     }
+//     log(err);
+// }
+// )
+
+writeFile(join(__dirname,"write.txt"),"ABC Company",(err) =>{
+    if(err){
     log(err);
-}
-)
+    }
+})
